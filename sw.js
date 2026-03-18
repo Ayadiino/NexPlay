1c09b11924c25bc4046bb8f6d0f613aa848035d1
Ayadiino <mhamadfaruq28@gmail.com>
2026-03-18 05:08:38 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'nexplay-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/nuke.html',
+  '/bara.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
